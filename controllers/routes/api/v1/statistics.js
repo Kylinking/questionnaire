b16542e4e8329d10eb2331de0fc110f4f07da1ca
kylin-Mac <kylinsmail@gmail.com>
2019-11-11 22:48:39 +0800

    更新分年级统计

--- controllers/routes/api/v1/statistics.js
+++ controllers/routes/api/v1/statistics.js
@@ -5,10 +5,21 @@ const Op = require('sequelize').Op;
 
 
 router.get('/statistics', async (req, res) => {
-    let { db, logger, admin } = { ...res.locals };
+    let {
+        db,
+        logger,
+        admin
+    } = {
+        ...res.locals
+    };
     let sequelize = db.sequelize;
+    let {
+        grade
+    } = {
+        ...req.query
+    };
     try {
-        if (!admin){
+        if (!admin) {
             res.json(util.MakeErrorResponse(`此用户无权限！`)).end();
             return;
         }
@@ -18,29 +29,35 @@ router.get('/statistics', async (req, res) => {
                 [sequelize.fn('SUM', sequelize.col('Submit')), 'Submit'],
             ]
         })).toJSON();
-        let classIds = (await db.ClassInfo.findAll({
-            attributes:['Id'],
-            where:{
-                'Grade':'2019'
-            },
-            raw:true
-        })).map(e=>e.Id);
-        let TotalAnswers = (await db.AnswerStatistics.findAll({
-            attributes:[
+
+        let statisticsCondition = {
+            attributes: [
                 [sequelize.fn('SUM', sequelize.col('ExtremelySatisfied')), 'ExtremelySatisfied'],
                 [sequelize.fn('SUM', sequelize.col('Satisfied')), 'Satisfied'],
                 [sequelize.fn('SUM', sequelize.col('Unsatisfied')), 'Unsatisfied'],
                 [sequelize.fn('SUM', sequelize.col('ExtremelyUnsatisfied')), 'ExtremelyUnsatisfied'],
                 'QuestionId'
             ],
-            include:[db.Question],
-            group:['QuestionId'],
-            where:{
-                'ClassId' :{
-                    [Op.in]:classIds
+            include: [db.Question],
+            group: ['QuestionId'],
+        };
+
+        if (grade) {
+            let classIds = (await db.ClassInfo.findAll({
+                attributes: ['Id'],
+                where: {
+                    Grade: grade
+                },
+                raw: true
+            })).map(e => e.Id);
+            statisticsCondition['where'] = {
+                ClassId: {
+                    [Op.in]: classIds
                 }
             }
-            }));
+        }
+
+        let TotalAnswers = (await db.AnswerStatistics.findAll(statisticsCondition));
 
         let FacultyCount = (await db.CountStatistics.findAll({
             attributes: [
@@ -48,34 +65,50 @@ router.get('/statistics', async (req, res) => {
                 [sequelize.fn('SUM', sequelize.col('Submit')), 'Submit'],
                 'FacultyId'
             ],
-            group:['FacultyId'],
-            include:[db.Faculty]
-        }))//.toJSON();
-        let FacultyAnswers = (await db.AnswerStatistics.findAll({
-            attributes:[
-                [sequelize.fn('SUM', sequelize.col('ExtremelySatisfied')), 'ExtremelySatisfied'],
-                [sequelize.fn('SUM', sequelize.col('Satisfied')), 'Satisfied'],
-                [sequelize.fn('SUM', sequelize.col('Unsatisfied')), 'Unsatisfied'],
-                [sequelize.fn('SUM', sequelize.col('ExtremelyUnsatisfied')), 'ExtremelyUnsatisfied'],
-                'FacultyId','QuestionId'
-            ],
-            group:['FacultyId','QuestionId'],
-            include:[db.Faculty,db.Question]
+            group: ['FacultyId'],
+            include: [db.Faculty]
         }));
-        res.json({Data:{
-            TotalAnswers,TotalCount,FacultyAnswers,FacultyCount
-        }}).end();
+        // let FacultyAnswers = (await db.AnswerStatistics.findAll({
+        //     attributes: [
+        //         [sequelize.fn('SUM', sequelize.col('ExtremelySatisfied')), 'ExtremelySatisfied'],
+        //         [sequelize.fn('SUM', sequelize.col('Satisfied')), 'Satisfied'],
+        //         [sequelize.fn('SUM', sequelize.col('Unsatisfied')), 'Unsatisfied'],
+        //         [sequelize.fn('SUM', sequelize.col('ExtremelyUnsatisfied')), 'ExtremelyUnsatisfied'],
+        //         'FacultyId', 'QuestionId'
+        //     ],
+        //     group: ['FacultyId', 'QuestionId'],
+        //     include: [db.Faculty, db.Question]
+        // }));
+        res.json({
+            Data: {
+                TotalAnswers,
+                TotalCount,
+                //FacultyAnswers,
+                FacultyCount
+            }
+        }).end();
     } catch (error) {
         logger.error(error);
     }
 })
 
 router.get('/statistics/:facultyId', async (req, res) => {
-    let { db, logger ,admin} = { ...res.locals };
+    let {
+        db,
+        logger,
+        admin
+    } = {
+        ...res.locals
+    };
+    let {
+        grade
+    } = {
+        ...req.query
+    };
     let facultyId = req.params.facultyId;
     let sequelize = db.sequelize;
     try {
-        if (!admin){
+        if (!admin) {
             res.json(util.MakeErrorResponse(`此用户无权限！`)).end();
             return;
         }
@@ -84,49 +117,76 @@ router.get('/statistics/:facultyId', async (req, res) => {
                 [sequelize.fn('SUM', sequelize.col('Total')), 'Total'],
                 [sequelize.fn('SUM', sequelize.col('Submit')), 'Submit'],
             ],
-            where:{
-                FacultyId:facultyId
+            where: {
+                FacultyId: facultyId
             }
         })).toJSON();
-        let TotalAnswers = (await db.AnswerStatistics.findAll({
-            attributes:[
-                [sequelize.fn('SUM', sequelize.col('ExtremelySatisfied')), 'ExtremelySatisfied'],
-                [sequelize.fn('SUM', sequelize.col('Satisfied')), 'Satisfied'],
-                [sequelize.fn('SUM', sequelize.col('Unsatisfied')), 'Unsatisfied'],
-                [sequelize.fn('SUM', sequelize.col('ExtremelyUnsatisfied')), 'ExtremelyUnsatisfied'],
-                'QuestionId'                
-            ],
-            where:{
-                FacultyId:facultyId
-            },
-            include:[db.Question],
-            group:['QuestionId']
-        }));
-        let ClassAnswers = (await db.AnswerStatistics.findAll({
-            attributes:[
-                [sequelize.fn('SUM', sequelize.col('ExtremelySatisfied')), 'ExtremelySatisfied'],
-                [sequelize.fn('SUM', sequelize.col('Satisfied')), 'Satisfied'],
-                [sequelize.fn('SUM', sequelize.col('Unsatisfied')), 'Unsatisfied'],
-                [sequelize.fn('SUM', sequelize.col('ExtremelyUnsatisfied')), 'ExtremelyUnsatisfied'],
-                'ClassId','QuestionId'
-            ],
-            where:{
-                FacultyId:facultyId
-            },
-            include:[db.ClassInfo,db.Question],
-            group:['ClassId','QuestionId',]
-        }));
-        let ClassCount = (await db.ClassInfo.findAll({
+
+        let statisticsCondition = {
             attributes: [
-                'Total','Submit','Id','Name'
+                [sequelize.fn('SUM', sequelize.col('ExtremelySatisfied')), 'ExtremelySatisfied'],
+                [sequelize.fn('SUM', sequelize.col('Satisfied')), 'Satisfied'],
+                [sequelize.fn('SUM', sequelize.col('Unsatisfied')), 'Unsatisfied'],
+                [sequelize.fn('SUM', sequelize.col('ExtremelyUnsatisfied')), 'ExtremelyUnsatisfied'],
+                'QuestionId'
             ],
-            where:{
-                FacultyId:facultyId
+            where: {
+                FacultyId: facultyId
             },
-        }))
-        res.json({Data:{
-            TotalAnswers,TotalCount,ClassAnswers,ClassCount
-        }}).end();
+            include: [db.Question],
+            group: ['QuestionId']
+        };
+        const classCountCondition = {
+            attributes: [
+                'Total', 'Submit', 'Id', 'Name'
+            ],
+            where: {
+                FacultyId: facultyId
+            },
+        };
+        if (grade) {
+            let classIds = (await db.ClassInfo.findAll({
+                attributes: ['Id'],
+                where: {
+                    Grade: grade
+                },
+                raw: true
+            })).map(e => e.Id);
+            statisticsCondition['where'] = {
+                ClassId: {
+                    [Op.in]: classIds
+                }
+            };
+            classCountCondition['where'].Id = {
+                [Op.in]: classIds
+            };
+        }
+        let TotalAnswers = (await db.AnswerStatistics.findAll(statisticsCondition));
+
+        // let ClassAnswers = (await db.AnswerStatistics.findAll({
+        //     attributes: [
+        //         [sequelize.fn('SUM', sequelize.col('ExtremelySatisfied')), 'ExtremelySatisfied'],
+        //         [sequelize.fn('SUM', sequelize.col('Satisfied')), 'Satisfied'],
+        //         [sequelize.fn('SUM', sequelize.col('Unsatisfied')), 'Unsatisfied'],
+        //         [sequelize.fn('SUM', sequelize.col('ExtremelyUnsatisfied')), 'ExtremelyUnsatisfied'],
+        //         'ClassId', 'QuestionId'
+        //     ],
+        //     where: {
+        //         FacultyId: facultyId
+        //     },
+        //     include: [db.ClassInfo, db.Question],
+        //     group: ['ClassId', 'QuestionId', ]
+        // }));
+
+        let ClassCount = (await db.ClassInfo.findAll(classCountCondition))
+        res.json({
+            Data: {
+                TotalAnswers,
+                TotalCount,
+                //ClassAnswers,
+                ClassCount
+            }
+        }).end();
     } catch (error) {
         logger.error(error);
     }
